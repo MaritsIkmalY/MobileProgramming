8a9c9d180a06bce30fbd1eef7abf5185bab45384
MaritsIkmalY <maritsikmaly@gmail.com>
2023-03-13 23:06:07 +0700

    flutter_layout_part_2

---
+++ flutter_layout_part_2/lib/detail_screen.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_layout/model/tourism_place.dart';
+
+class DetailScreen extends StatelessWidget {
+  const DetailScreen({Key? key, required this.place}) : super(key: key);
+
+  final TourismPlace place;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(children: <Widget>[
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Image.asset(
+                place.imageAsset,
+                scale: 1,
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 16.0),
+                child: Text(place.name,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      fontSize: 30.0,
+                      fontFamily: 'Poppins',
+                    )),
+              ), //Container For Title
+              Container(
+                margin: const EdgeInsets.symmetric(vertical: 16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Column(
+                      children: <Widget>[
+                        Icon(Icons.calendar_today),
+                        Text(place.day),
+                      ],
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Icon(Icons.access_time),
+                        Text(place.time),
+                      ],
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Icon(Icons.attach_money),
+                        Text(place.cost),
+                      ],
+                    ),
+                  ],
+                ),
+              ), //Container for Icons
+              Container(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(
+                  place.description,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    fontFamily: 'verdana',
+                  ),
+                ),
+              ), //Container For Description
+              SizedBox(
+                height: 150,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(20.0),
+                        child: Image.asset(
+                          place.imageAsset2,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(20.0),
+                        child: Image.asset(
+                          place.imageAsset3,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(20.0),
+                        child: Image.asset(
+                          place.imageAsset4,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(20.0),
+                        child: Image.asset(
+                          place.imageAsset5,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ), //Container For ListView
+            ],
+          ),
+        ]),
+      ),
+    );
+  }
+}
